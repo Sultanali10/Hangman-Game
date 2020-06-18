1d54cada069b84df484a918b8720d4b44f8f9aac
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-18 03:34:37 +0300

    get word from array, get word's characters number then add "-" for each character in a new array finally create a string of "---" from the new array using joined and showing it on the screen as a label text

--- Hangman Game/ViewController.swift
+++ Hangman Game/ViewController.swift
@@ -11,6 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     var wordsArray = [String]()
+    var word:String?
+    var wordCharct = [Character]()
+    var jointArrayForLabel = [String]()
+    
     
     @IBOutlet var answerLabel: UILabel!
     @IBOutlet var textField: UITextField!
@@ -21,6 +25,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         getWords()
+        getWordFromArray()
+        
+    }
+    
+    func getWordFromArray(){
+        wordsArray.shuffle()
+        guard let word = wordsArray.first else {return}
+        let upperWord = word.uppercased()
+        
+        for char in upperWord {
+            wordCharct.append(char)
+        }
+        
+        for _ in wordCharct{
+            jointArrayForLabel.append("-")
+        }
+        
+        let joined = jointArrayForLabel.joined()
+        
+        answerLabel.text = joined
+        answerLabel.textColor = .red
+        
+        print(wordCharct)
+        print(joined)
     }
     
     func getWords(){
