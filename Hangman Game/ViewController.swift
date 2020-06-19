fcb5ca752470e10cfc831a72d4bd3305b6adcbf5
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-20 02:53:52 +0300

    Day 41 Hangman Game Challenge Completed

--- Hangman Game/ViewController.swift
+++ Hangman Game/ViewController.swift
@@ -18,32 +18,58 @@ class ViewController: UIViewController , UITextFieldDelegate {
     var dashedWord:String?
     var newAnswer:String?
     var wordArray = [Character]()
-     
+    var attempts = 7
+    
+    
     @IBOutlet var answerLabel: UILabel!
     @IBOutlet var textField: UITextField!
     @IBOutlet var checkButton: UIButton!
+    
     @IBAction func checkPressed(_ sender: UIButton) {
-        textField.resignFirstResponder()
-        guard let unwrapedUserCharc = textField.text else {return}
-        userCharc = Character(unwrapedUserCharc.uppercased())
-        checkWordAvailable(my: userCharc!)
+        if textField.text != "" {
+            guard let unwrapedUserCharc = textField.text else {return}
+            userCharc = Character(unwrapedUserCharc.uppercased())
+            checkWordAvailable(my: userCharc!)
+            textField.text = ""
+        } else {
+            let ac = UIAlertController(title: "No Characters", message: "Please enter a character first", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
     }
     
     func checkWordAvailable(my newCharch: Character){
         for (position , charc) in word!.enumerated(){
-            var i = 0
             if charc == userCharc {
-//            print("positon of \(charc) is \(position)")
-//                print(wordArray)
                 wordArray[position] = charc
                 newAnswer = String(wordArray)
-//                print(newAnswer!)
             }
-            i += 1
         }
-        answerLabel.text = newAnswer
+        if newAnswer == answerLabel.text {
+            attempts -= 1
+            if attempts >= 1 {
+                let ac = UIAlertController(title: "Oops", message: "This character is not part of the word, try another one. You have \(attempts) left", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default))
+                present(ac, animated: true)
+            } else {
+                let ac = UIAlertController(title: "Game Over", message: "You failed to discover the word, It was \(word!). Lets try another word :)", preferredStyle: .alert)
+                ac.addAction(UIAlertAction(title: "OK", style: .default) { _ in
+                    self.getWordFromArray()
+                })
+                present(ac, animated: true)
+            }
+        } else if newAnswer == word {
+            let ac = UIAlertController(title: "Congratulations", message: "You have discovered the word successfully. Lets have fun with another word", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            
+        } else {
+            answerLabel.text = newAnswer
+        }
+        
     }
-
+    // this code used to accept only one character in textfield
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         guard let textFieldText = textField.text,
             let rangeOfTextToReplace = Range(range, in: textFieldText) else {
@@ -54,51 +80,52 @@ class ViewController: UIViewController , UITextFieldDelegate {
         return count <= 1
     }
     
-  func hideKeyboardWhenTappedAround() {
-      let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
-      view.addGestureRecognizer(tap)
-
-  }
-
-  @objc func dismissKeyboard() {
-      view.endEditing(true)
-  }
-
+    // to hide keyboard when tap in any empty space on the page
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getWords()
         getWordFromArray()
         hideKeyboardWhenTappedAround()
         checkButton.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(checkPressed)))
-
     }
     
+    
+    // get a word from the array of words then create dashes from it then show dashes on the app
     func getWordFromArray(){
         wordsArray.shuffle()
         guard let worde = wordsArray.first else {return}
         let upperWord = worde.uppercased()
         word = upperWord
+        print(word!)
+        wordCharct.removeAll()
         for char in upperWord {
             wordCharct.append(char)
         }
         
+        jointArrayForLabel.removeAll()
         for _ in wordCharct{
             jointArrayForLabel.append("-")
         }
-        
-         dashedWord = jointArrayForLabel.joined()
-        
+        dashedWord = ""
+        dashedWord = jointArrayForLabel.joined()
         answerLabel.text = dashedWord
         newAnswer = dashedWord
         wordArray = Array(dashedWord!)
         answerLabel.textColor = .red
         answerLabel.layer.borderWidth = 1
         answerLabel.layer.borderColor = UIColor.purple.cgColor
-        
-        print(wordCharct)
-        print(dashedWord!)
     }
     
+    // get words array from the file
     func getWords(){
         if let url = Bundle.main.url(forResource: "words", withExtension: "txt"){
             if let urlString = try? String(contentsOf: url){
